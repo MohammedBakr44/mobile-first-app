9ac99f3b9ab55ac23deb6414e6f1999e8da3556f
MohammedBakr44 <mohammed.mobile.tricks@gmail.com>
2019-08-27 04:43:00 +0200

    README edited

--- src/shared/ts/pics.ts
+++ src/shared/ts/pics.ts
@@ -1,8 +1,8 @@
 import Unsplash, { toJson } from 'unsplash-js';
 
 export const unsplash = new Unsplash({
-    applicationId: "YOUR_APP_KEY",
-    secret: "YOUR_SCERET_KEY"
+    applicationId: "YOUR_APPLICATION_ID",
+    secret: "YOUR_SECRET_ID"
 });
 
 export const defaultConfig: { query: string, count: number } = { query: "future", count: 10 };
